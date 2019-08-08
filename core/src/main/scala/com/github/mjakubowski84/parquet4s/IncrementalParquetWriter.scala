9c2a4742348fda3fc342c511266c69a4882b2655
Alex Klibisz <aklibisz@cibotechnologies.com>
2019-08-08 11:16:38 -0400

    throwing a more descriptive exception when calling write on a closed writer

--- core/src/main/scala/com/github/mjakubowski84/parquet4s/IncrementalParquetWriter.scala
+++ core/src/main/scala/com/github/mjakubowski84/parquet4s/IncrementalParquetWriter.scala
@@ -3,6 +3,7 @@ package com.github.mjakubowski84.parquet4s
 import java.io.Closeable
 
 import org.apache.hadoop.fs.Path
+import org.slf4j.LoggerFactory
 
 /**
   * Interface for a writer which can handle multiple calls to .write with a single call to .close.
@@ -20,16 +21,37 @@ object IncrementalParquetWriter {
     * @param options configuration of how Parquet files should be created and written
     * @tparam T schema of data to write
     */
-  def apply[T: ParquetRecordEncoder : ParquetSchemaResolver](path: String, options: ParquetWriter.Options = ParquetWriter.Options()): IncrementalParquetWriter[T] = {
+  def apply[T: ParquetRecordEncoder: ParquetSchemaResolver](
+      path: String,
+      options: ParquetWriter.Options = ParquetWriter.Options())
+    : IncrementalParquetWriter[T] = {
     new IncrementalParquetWriter[T] {
-      private val writer = ParquetWriter.internalWriter(new Path(path), ParquetSchemaResolver.resolveSchema[T], options)
+      private val writer = ParquetWriter.internalWriter(
+        new Path(path),
+        ParquetSchemaResolver.resolveSchema[T],
+        options)
       private val valueCodecConfiguration = options.toValueCodecConfiguration
+      private val logger = LoggerFactory.getLogger(this.getClass)
+      private var isClosed: Boolean = false
+
       override def write(data: Iterable[T]): Unit =
-        data.foreach { elem =>
-          writer.write(ParquetRecordEncoder.encode[T](elem, valueCodecConfiguration))
+        if (isClosed)
+          throw new IllegalStateException(
+            "Attempted to write with a writer which was already closed")
+        else
+          data.foreach { elem =>
+            writer.write(
+              ParquetRecordEncoder.encode[T](elem, valueCodecConfiguration))
+          }
+
+      override def close(): Unit = this.synchronized {
+        if (isClosed)
+          logger.warn("Attempted to close a writer which was already closed")
+        else {
+          isClosed = true
+          writer.close()
         }
-      override def close(): Unit = writer.close()
+      }
     }
   }
 }
-
