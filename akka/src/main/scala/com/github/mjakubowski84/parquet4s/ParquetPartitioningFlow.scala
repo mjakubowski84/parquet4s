cf13f004374d0dba59a83a0c4e65db4974fb1697
Marcin Jakubowski <mjakubowski84@gmail.com>
2020-04-27 08:46:00 +0200

    Builder for ParquetPartitioningFlow. StringLens implementation.

--- akka/src/main/scala/com/github/mjakubowski84/parquet4s/ParquetPartitioningFlow.scala
+++ akka/src/main/scala/com/github/mjakubowski84/parquet4s/ParquetPartitioningFlow.scala
@@ -3,30 +3,58 @@ package com.github.mjakubowski84.parquet4s
 
 import java.nio.file.Paths
 import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 import akka.stream.stage._
 import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration.FiniteDuration
+import org.apache.parquet.hadoop.{ParquetWriter => HadoopParquetWriter}
 
 object ParquetPartitioningFlow {
-  def apply[T: ParquetRecordEncoder : ParquetSchemaResolver](
-                                                              basePath: String,
-                                                              maxCount: Long,
-                                                              maxDuration: FiniteDuration,
-                                                              writeOptions: ParquetWriter.Options = ParquetWriter.Options()
-                                                            ): GraphStage[FlowShape[T, T]] =
-    new ParquetPartitioningFlow[T](basePath, maxCount, maxDuration, writeOptions)
+
+  val DefaultMaxCount: Long = HadoopParquetWriter.DEFAULT_BLOCK_SIZE
+  val DefaultMaxDuration: FiniteDuration = FiniteDuration(1, TimeUnit.MINUTES)
+
+
+  def builder[T: ParquetRecordEncoder : ParquetSchemaResolver](basePath: String): Builder[T] =
+    BuilderImpl(basePath)
+
+  trait Builder[T] {
+    def withMaxCount(maxCount: Long): Builder[T]
+    def withMaxDuration(maxDuration: FiniteDuration): Builder[T]
+    def withWriteOptions(writeOptions: ParquetWriter.Options): Builder[T]
+    def withPartitionBy(partitionBy: Seq[T => String]): Builder[T]
+    def addPartitionBy(partitionBy: T => String): Builder[T]
+    def build(): GraphStage[FlowShape[T, T]]
+  }
+
+  private case class BuilderImpl[T: ParquetRecordEncoder : ParquetSchemaResolver](
+                        basePath: String,
+                        maxCount: Long = DefaultMaxCount,
+                        maxDuration: FiniteDuration = DefaultMaxDuration,
+                        writeOptions: ParquetWriter.Options = ParquetWriter.Options(),
+                        partitionBy: Seq[T => String] = Seq.empty
+                      ) extends Builder[T] {
+
+    def withMaxCount(maxCount: Long): Builder[T] = copy(maxCount = maxCount)
+    def withMaxDuration(maxDuration: FiniteDuration): Builder[T] = copy(maxDuration = maxDuration)
+    def withWriteOptions(writeOptions: ParquetWriter.Options): Builder[T] = copy(writeOptions = writeOptions)
+    def withPartitionBy(partitionBy: Seq[T => String]): Builder[T] = copy(partitionBy = partitionBy)
+    def addPartitionBy(partitionBy: T => String): Builder[T] = copy(partitionBy = this.partitionBy :+ partitionBy)
+    def build(): GraphStage[FlowShape[T, T]] = new ParquetPartitioningFlow(basePath, maxCount, maxDuration, writeOptions)
+  }
+
 }
 
 
 private class ParquetPartitioningFlow[T: ParquetRecordEncoder : ParquetSchemaResolver](
-                                                                          basePath: String,
-                                                                            maxCount: Long,
-                                                                            maxDuration: FiniteDuration,
-                                                                            writeOptions: ParquetWriter.Options
-                               ) extends GraphStage[FlowShape[T, T]] {
+                                                                                        basePath: String,
+                                                                                        maxCount: Long,
+                                                                                        maxDuration: FiniteDuration,
+                                                                                        writeOptions: ParquetWriter.Options
+                                                                                      ) extends GraphStage[FlowShape[T, T]] {
   val in: Inlet[T] = Inlet[T]("ParquetPartitioningFlow.in")
   val out: Outlet[T] = Outlet[T]("ParquetPartitioningFlow.out")
   val shape: FlowShape[T, T] = FlowShape.of(in, out)
