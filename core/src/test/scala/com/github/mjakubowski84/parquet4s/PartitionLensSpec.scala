f506b4958ae06db007bfaabef29238ed075c76f3
Marcin Jakubowski <mjakubowski84@gmail.com>
2020-07-11 22:09:06 +0200

    Generic SkippingParquetRecordEncoder and SkippingParquetSchemaResolver.

--- core/src/test/scala/com/github/mjakubowski84/parquet4s/PartitionLensSpec.scala
+++ core/src/test/scala/com/github/mjakubowski84/parquet4s/PartitionLensSpec.scala
@@ -1,5 +1,6 @@
 package com.github.mjakubowski84.parquet4s
 
+import com.github.mjakubowski84.parquet4s.ValueImplicits._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -15,6 +16,18 @@ class PartitionLensSpec extends AnyFlatSpec with Matchers {
     address = Address(
       street = Street(name = "Broad St", more = Some("123")),
       city = "Somewhere"
+    )
+  )
+
+  val personRecord: RowParquetRecord = RowParquetRecord(
+    "name" -> "Joe",
+    "age" -> 18,
+    "address" -> RowParquetRecord(
+      "street" -> RowParquetRecord(
+        "name" -> "Broad St",
+        "more" -> Some("123")
+      ),
+      "city" -> "Somewhere"
     )
   )
 
@@ -55,7 +68,47 @@ class PartitionLensSpec extends AnyFlatSpec with Matchers {
 
   it should "fail to extract a field using empty path" in {
     val e = intercept[IllegalArgumentException](PartitionLens[Person](person, ""))
-    e.getMessage should be(s"Invalid element at path ''. Field '' does not exist.")
+    e.getMessage should be(s"Invalid path ''. Field '' does not exist.")
+  }
+
+  "Generic PartitionLens" should "extract String field from a long path" in {
+    PartitionLens[RowParquetRecord](personRecord, "address.street.name") should be(("address.street.name", "Broad St"))
+  }
+
+  it should "extract String field from a short path" in {
+    PartitionLens[RowParquetRecord](personRecord, "name") should be(("name", "Joe"))
+  }
+
+  it should "fail to extract Option field" in {
+    val path = "address.street.more"
+    val e = intercept[IllegalArgumentException](PartitionLens[RowParquetRecord](personRecord, path))
+    e.getMessage should be(s"Invalid path '$path'. Only String field can be used for partitioning.")
+  }
+
+  it should "fail to extract non-String field" in {
+    val path = "age"
+    val e = intercept[IllegalArgumentException](PartitionLens[RowParquetRecord](personRecord, path))
+    e.getMessage should be(s"Invalid path '$path'. Only String field can be used for partitioning.")
+  }
+
+  it should "fail to extract non-existing field" in {
+    val e = intercept[IllegalArgumentException](PartitionLens[RowParquetRecord](personRecord, "address.state"))
+    e.getMessage should be(s"Invalid path 'address.state'. Field 'address.state' does not exist.")
+  }
+
+  it should "fail to read a child from a String field" in {
+    val e = intercept[IllegalArgumentException](PartitionLens[RowParquetRecord](personRecord, "name.[0]"))
+    e.getMessage should be(s"Invalid path 'name.[0]'. Field 'name.[0]' does not exist.")
+  }
+
+  it should "fail to extract Product" in {
+    val e = intercept[IllegalArgumentException](PartitionLens[RowParquetRecord](personRecord, "address.street"))
+    e.getMessage should be(s"Invalid path 'address.street'. Only String field can be used for partitioning.")
+  }
+
+  it should "fail to extract a field using empty path" in {
+    val e = intercept[IllegalArgumentException](PartitionLens[RowParquetRecord](personRecord, ""))
+    e.getMessage should be(s"Invalid path ''. Field '' does not exist.")
   }
 
 }
--- core/src/test/scala/com/github/mjakubowski84/parquet4s/SkippingParquetSchemaResolverSpec.scala
+++ core/src/test/scala/com/github/mjakubowski84/parquet4s/SkippingParquetSchemaResolverSpec.scala
@@ -3,7 +3,7 @@ package com.github.mjakubowski84.parquet4s
 import com.github.mjakubowski84.parquet4s.SkippingParquetSchemaResolver.resolveSchema
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.{BINARY, INT32}
 import org.apache.parquet.schema.Type.Repetition.{OPTIONAL, REQUIRED}
-import org.apache.parquet.schema.{OriginalType, Types}
+import org.apache.parquet.schema.{MessageType, OriginalType, Types}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -13,7 +13,24 @@ class SkippingParquetSchemaResolverSpec extends AnyFlatSpec with Matchers {
   case class Address(street: Street, city: String)
   case class Person(name: String, age: Int, address: Address)
 
-  "SkippingParquetSchemaResolver" should "skip a field at a long path" in {
+  implicit val fullSchema: MessageType = Message(
+    Some(classOf[Person].getCanonicalName),
+    Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("name"),
+    Types.primitive(INT32, REQUIRED).as(OriginalType.INT_32).named("age"),
+    Types.optionalGroup()
+      .addField(Types.optionalGroup()
+        .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("name"))
+        .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("more"))
+        .named("street"))
+      .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("city"))
+      .named("address")
+  )
+
+  "SkippingParquetSchemaResolver" should "create unpartitioned schema" in {
+    resolveSchema[Person](Set.empty) should be(fullSchema)
+  }
+
+  it should "skip a field at a long path" in {
     resolveSchema[Person](Set("address.street.name")) should be(Message(
       Some(classOf[Person].getCanonicalName),
       Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("name"),
@@ -57,6 +74,62 @@ class SkippingParquetSchemaResolverSpec extends AnyFlatSpec with Matchers {
   it should "process empty class" in {
     case class Empty()
     resolveSchema[Empty](Set("field")) should be(Message(None))
+  }
+
+  "Generic SkippingParquetSchemaResolver" should "create unpartitioned schema" in {
+    resolveSchema[RowParquetRecord](Set.empty) should be(fullSchema)
+  }
+
+  it should "skip a field at a long path" in {
+    resolveSchema[RowParquetRecord](Set("address.street.name")) should be(Message(
+      Some(classOf[Person].getCanonicalName),
+      Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("name"),
+      Types.primitive(INT32, REQUIRED).as(OriginalType.INT_32).named("age"),
+      Types.optionalGroup()
+        .addField(Types.optionalGroup()
+          .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("more"))
+          .named("street"))
+        .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("city"))
+        .named("address")
+    ))
+  }
+
+  it should "skip a mid path" in {
+    resolveSchema[RowParquetRecord](Set("address.street")) should be(Message(
+      Some(classOf[Person].getCanonicalName),
+      Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("name"),
+      Types.primitive(INT32, REQUIRED).as(OriginalType.INT_32).named("age"),
+      Types.optionalGroup()
+        .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("city"))
+        .named("address")
+    ))
+  }
+
+  it should "skip all fields of case class" in {
+    resolveSchema[RowParquetRecord](Set("address.street.name", "address.street.more")) should be(Message(
+      Some(classOf[Person].getCanonicalName),
+      Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("name"),
+      Types.primitive(INT32, REQUIRED).as(OriginalType.INT_32).named("age"),
+      Types.optionalGroup()
+        .addField(Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("city"))
+        .named("address")
+    ))
+  }
+
+  it should "skip the only field from a simple case class" in {
+    val simpleMessage: MessageType = Message(
+      Some("Simple"),
+      Types.primitive(BINARY, OPTIONAL).as(OriginalType.UTF8).named("field")
+    )
+    implicit val resolver: SkippingParquetSchemaResolver[RowParquetRecord] =
+      RowParquetRecord.genericSkippingParquetSchemaResolver(simpleMessage)
+    resolveSchema[RowParquetRecord](Set("field")) should be(Message(Some("Simple")))
+  }
+
+  it should "process empty class" in {
+    implicit val resolver: SkippingParquetSchemaResolver[RowParquetRecord] =
+      RowParquetRecord.genericSkippingParquetSchemaResolver(Message(Some("Empty")))
+    resolveSchema[RowParquetRecord](Set("field")) should be(Message(Some("Empty")))
   }
 
 }
