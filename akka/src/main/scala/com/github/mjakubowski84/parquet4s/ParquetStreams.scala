030aacaafdb595156c68a972bbb8d3b1821218ef
Marcin Jakubowski <mjakubowski84@gmail.com>
2020-10-11 11:57:14 +0200

    new akka source builder that allows to define projection over parquet files

--- akka/src/main/scala/com/github/mjakubowski84/parquet4s/ParquetStreams.scala
+++ akka/src/main/scala/com/github/mjakubowski84/parquet4s/ParquetStreams.scala
@@ -39,12 +39,34 @@ object ParquetStreams {
     *           {{{ case class MyData(id: Long, name: String, created: java.sql.Timestamp) }}}
     * @return The source of Parquet data
     */
+  @deprecated(message = "Use another fromParquet version that allows to do more", since = "1.6.0")
   def fromParquet[T : ParquetRecordDecoder](
                                              path: String,
                                              options: ParquetReader.Options = ParquetReader.Options(),
                                              filter: Filter = Filter.noopFilter
                                            ): Source[T, NotUsed] =
-    ParquetSource(new Path(path), options, filter)
+    ParquetSource(new Path(path), options, filter, None)
+
+  /**
+   * Creates a [[akka.stream.scaladsl.Source]] that reads Parquet data from the specified path.
+   * If there are multiple files at path then the order in which files are loaded is determined by underlying
+   * filesystem.
+   * <br/>
+   * Path can refer to local file, HDFS, AWS S3, Google Storage, Azure, etc.
+   * Please refer to Hadoop client documentation or your data provider in order to know how to configure the connection.
+   * <br/>
+   * Can read also <b>partitioned</b> directories. Filter applies also to partition values. Partition values are set
+   * as fields in read entities at path defined by partition name. Path can be a simple column name or a dot-separated
+   * path to nested field. Missing intermediate fields are automatically created for each read record.
+   * <br/>
+   * Allows to turn on a <b>projection</b> over original file schema in order to boost read performance if not all
+   * columns are required to be read.
+   *
+   * @tparam T type of data that represent the schema of the Parquet data, e.g.:
+   *           {{{ case class MyData(id: Long, name: String, created: java.sql.Timestamp) }}}
+   * @return Builder of the source.
+   */
+  def fromParquet[T]: ParquetSource.Builder[T] = ParquetSource[T]
 
   /**
     * Creates a [[akka.stream.scaladsl.Sink]] that writes Parquet data to single file at the specified path (including
@@ -162,7 +184,7 @@ object ParquetStreams {
    * </ol>
    * @param path URI to Parquet files, e.g.: {{{ "file:///data/users" }}}
    * @tparam T type of message that flow is meant to accept
-   * @return Builder of [[ParquetPartitioningFlow]]
+   * @return Builder of the flow.
    */
   def viaParquet[T](path: String): ParquetPartitioningFlow.Builder[T, T] =
     ParquetPartitioningFlow.builder(new Path(path))
