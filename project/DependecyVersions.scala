a0bf3f5abe717f6a7d147ff3cc7c68f276ce607e
Marcin Jakubowski <mjakubowski84@gmail.com>
2021-04-17 22:44:34 +0200

    Use Spark 3.1.1 for compatibility testing and drop Spark it:tests in Scala 2.11

--- project/DependecyVersions.scala
+++ project/DependecyVersions.scala
@@ -1,6 +1,6 @@
 object DependecyVersions {
   val parquetVersion = "1.12.0"
-  val sparkVersion = "2.4.7"
+  val sparkVersion = "3.1.1"
   val hadoopVersion = "2.9.2"
   val slf4jVersion = "1.7.30"
   val akkaVersion = "2.5.32"
