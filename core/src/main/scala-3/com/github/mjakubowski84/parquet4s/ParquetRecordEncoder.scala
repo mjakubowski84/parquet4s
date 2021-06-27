6dd845e87eef750f2c3ea1b01067d0145ff30c58
Marcin Jakubowski <mjakubowski84@gmail.com>
2021-06-27 21:51:46 +0200

    Fixing performance of writer

--- core/src/main/scala-3/com/github/mjakubowski84/parquet4s/ParquetRecordEncoder.scala
+++ core/src/main/scala-3/com/github/mjakubowski84/parquet4s/ParquetRecordEncoder.scala
@@ -4,6 +4,7 @@ package com.github.mjakubowski84.parquet4s
 import scala.deriving.Mirror
 import scala.annotation.implicitNotFound
 import scala.util.control.NonFatal
+import scala.reflect.ClassTag
 
 
 /**
@@ -16,10 +17,11 @@ import scala.util.control.NonFatal
 trait ParquetRecordEncoder[T]:
   /**
    * @param entity        data to be encoded
+   * @param resolver      data type used to resolve and cache empty instances of [[RowParquetRecord]] for given class
    * @param configuration [ValueCodecConfiguration] used by some codecs
    * @return [[RowParquetRecord]] containing product elements from the data
    */
-  def encode(entity: T, configuration: ValueCodecConfiguration): RowParquetRecord
+  def encode(entity: T, resolver: EmptyRowParquetRecordResolver, configuration: ValueCodecConfiguration): RowParquetRecord
 
 object ParquetRecordEncoder:
 
@@ -37,11 +39,13 @@ object ParquetRecordEncoder:
   def apply[T](using ev: ParquetRecordEncoder[T]): ParquetRecordEncoder[T] = ev
 
   def encode[T](entity: T, configuration: ValueCodecConfiguration = ValueCodecConfiguration.Default)
-               (using ev: ParquetRecordEncoder[T]): RowParquetRecord = ev.encode(entity, configuration)
+               (using ev: ParquetRecordEncoder[T]): RowParquetRecord = ev.encode(entity, null, configuration)
 
   given ParquetRecordEncoder[Fields[EmptyTuple, EmptyTuple]] with
-    def encode(empty: Fields[EmptyTuple, EmptyTuple], configuration: ValueCodecConfiguration): RowParquetRecord = 
-      RowParquetRecord.EmptyNoSchema
+    def encode(empty: Fields[EmptyTuple, EmptyTuple],
+               resolver: EmptyRowParquetRecordResolver,
+               configuration: ValueCodecConfiguration): RowParquetRecord =
+      resolver.resolveEmptyRowParquetRecord
 
   given [L <: String & Singleton: ValueOf,
          LT <: Tuple,
@@ -49,7 +53,9 @@ object ParquetRecordEncoder:
          VT <: Tuple
         ](using tailEncoder: ParquetRecordEncoder[Fields[LT, VT]])
         : ParquetRecordEncoder[Fields[L *: LT, V *: VT]] with
-    def encode(fields: Fields[L *: LT, V *: VT], configuration: ValueCodecConfiguration): RowParquetRecord =
+    def encode(fields: Fields[L *: LT, V *: VT],
+               resolver: EmptyRowParquetRecordResolver,
+               configuration: ValueCodecConfiguration): RowParquetRecord =
       val fieldName = summon[ValueOf[L]].value
       val fieldValue = try {
         summon[ValueEncoder[V]].encode(fields.values.head, configuration)
@@ -57,13 +63,45 @@ object ParquetRecordEncoder:
         case NonFatal(cause) =>
           throw EncodingException(s"Failed to encode field $fieldName: ${fields.values.head}, due to ${cause.getMessage}", cause)
       }
-      tailEncoder.encode(Fields(fields.values.tail), configuration).prepended(fieldName, fieldValue)
+      tailEncoder
+        .encode(Fields(fields.values.tail), resolver.add(fieldName), configuration)
+        .add(fieldName, fieldValue)
     
   given derived[P <: Product](using
+                              classTag: ClassTag[P],
                               mirror: Mirror.ProductOf[P],
                               encoder: ParquetRecordEncoder[Fields[mirror.MirroredElemLabels, mirror.MirroredElemTypes]]
                              ): ParquetRecordEncoder[P] with
-    def encode(product: P, configuration: ValueCodecConfiguration): RowParquetRecord =
-      encoder.encode(Fields(Tuple.fromProductTyped(product)), configuration)
+    def encode(product: P, resolver: EmptyRowParquetRecordResolver, configuration: ValueCodecConfiguration): RowParquetRecord =
+      encoder.encode(
+        Fields(Tuple.fromProductTyped(product)),
+        EmptyRowParquetRecordResolver(classTag.runtimeClass),
+        configuration
+      )
 
 end ParquetRecordEncoder
+
+sealed trait EmptyRowParquetRecordResolver:
+  def add(fieldName: String): EmptyRowParquetRecordResolver
+  def resolveEmptyRowParquetRecord: RowParquetRecord
+
+object EmptyRowParquetRecordResolver:
+  private[parquet4s] val Cache = scala.collection.concurrent.TrieMap.empty[Class[_], RowParquetRecord]
+
+  def apply(clazz: Class[_]): EmptyRowParquetRecordResolver =
+    Cache.get(clazz) match {
+      case Some(record) => Cached(record)
+      case None => Builder(Vector.empty, clazz)
+    }
+
+  private class Cached(rowParquetRecord: RowParquetRecord) extends EmptyRowParquetRecordResolver:
+    override def add(fieldName: String): EmptyRowParquetRecordResolver = this
+    override def resolveEmptyRowParquetRecord: RowParquetRecord = rowParquetRecord
+
+  private class Builder(fields: Vector[String], clazz: Class[_]) extends EmptyRowParquetRecordResolver:
+    override def add(fieldName: String): EmptyRowParquetRecordResolver =
+      Builder(fields :+ fieldName, clazz)
+    override def resolveEmptyRowParquetRecord: RowParquetRecord =
+      Cache.getOrElseUpdate(clazz, RowParquetRecord.emptyWithSchema(fields))
+
+end EmptyRowParquetRecordResolver
