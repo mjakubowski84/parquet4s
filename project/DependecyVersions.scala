e96df47cc7be385dea1c3a182b3cd714f440a2c7
Marcin Jakubowski <mjakubowski84@gmail.com>
2021-11-11 11:28:18 +0100

    upgrading parquet-hadoop to 2.12.2 and provided hadoop-client to 3.3.1

--- project/DependecyVersions.scala
+++ project/DependecyVersions.scala
@@ -1,8 +1,8 @@
 object DependecyVersions {
-  val parquetVersion   = "1.12.1"
+  val parquetVersion   = "1.12.2"
   val shapelessVersion = "2.3.7"
   val sparkVersion     = "3.2.0"
-  val hadoopVersion    = "2.9.2"
+  val hadoopVersion    = "3.3.1"
   val slf4jVersion     = "1.7.32"
   val akkaVersion      = "2.6.17"
   val fs2Version       = "3.1.6"
