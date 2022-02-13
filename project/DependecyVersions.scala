3fbc3fb52963acebfebfb35f8e482753b483610e
Marcin Jakubowski <mjakubowski84@gmail.com>
2022-02-13 19:11:36 +0100

    minor upgrade of shapeless

--- project/DependecyVersions.scala
+++ project/DependecyVersions.scala
@@ -1,6 +1,6 @@
 object DependecyVersions {
   val parquetVersion    = "1.12.2"
-  val shapelessVersion  = "2.3.7"
+  val shapelessVersion  = "2.3.8"
   val sparkVersion      = "3.2.1"
   val hadoopVersion     = "3.3.1"
   val slf4jVersion      = "1.7.35"
