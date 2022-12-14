331373bbe62fbce5322305e10f04fda9c12b6eca
Marcin Jakubowski <mjakubowski84@gmail.com>
2022-12-14 09:37:41 +0100

    Dependency updates and FS2 improvements

--- project/DependecyVersions.scala
+++ project/DependecyVersions.scala
@@ -1,10 +1,12 @@
 object DependecyVersions {
-  val parquetVersion    = "1.12.3"
-  val shapelessVersion  = "2.3.10"
-  val sparkVersion      = "3.3.1"
-  val hadoopVersion     = "3.3.2"
-  val slf4jVersion      = "2.0.3"
-  val akkaVersion       = "2.6.20"
-  val fs2Version        = "3.3.0"
-  val catsEffectVersion = "3.3.14"
+  val parquetVersion               = "1.12.3"
+  val shapelessVersion             = "2.3.10"
+  val sparkVersion                 = "3.3.1"
+  val hadoopVersion                = "3.3.4"
+  val slf4jVersion                 = "2.0.5"
+  val logbackVersion               = "1.3.5" // stick to 1.3.x for JDK-8 compatibility
+  val akkaVersion                  = "2.6.20" // non-licensed version
+  val fs2Version                   = "3.4.0"
+  val catsEffectVersion            = "3.4.2"
+  val scalaCollectionCompatVersion = "2.9.0"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.13")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.15")
 addSbtPlugin("com.thoughtworks.sbt-api-mappings" % "sbt-api-mappings" % "3.0.2")
 addSbtPlugin("pl.project13.scala" % "sbt-jmh" % "0.4.3")
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.6")
-addSbtPlugin("com.47deg" % "sbt-microsites" % "1.3.4")
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.0")
+addSbtPlugin("com.47deg" % "sbt-microsites" % "1.4.0")
 
-libraryDependencies ++= Seq("com.47deg" %% "github4s" % "0.28.5")
+//libraryDependencies ++= Seq("com.47deg" %% "github4s" % "0.28.5")
