85536904ab1504a451c12381d0566ebcf46656c6
Hua Jiang <huajiang@tubi.tv>
2023-08-03 16:52:55 +0800

    scalafmt

--- core/src/main/scala/com/github/mjakubowski84/parquet4s/InMemoryInputFile.scala
+++ core/src/main/scala/com/github/mjakubowski84/parquet4s/InMemoryInputFile.scala
@@ -54,7 +54,7 @@ class InMemoryInputFile private (content: Array[Byte]) extends InputFile {
     override def read(): Int =
       if (remaining == 0) -1
       else {
-        val next = content(pos) & 0xFF
+        val next = content(pos) & 0xff
         pos += 1
         next
       }
