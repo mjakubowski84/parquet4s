dc76fdfec2a7807c4e3d594a74cda588057a193c
Marcin Jakubowski <mjakubowski84@gmail.com>
2024-02-05 19:07:02 +0100

    Bugfix: support reading decimals encoded as long values

---
+++ core/src/test/scala/com/github/mjakubowski84/parquet4s/DecimalValueSpec.scala
@@ -0,0 +1,60 @@
+package com.github.mjakubowski84.parquet4s
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.apache.parquet.schema.PrimitiveType
+import org.apache.parquet.schema.LogicalTypeAnnotation
+
+import scala.util.Using
+
+class DecimalValueSpec extends AnyFlatSpec with Matchers {
+
+  case class Data(decimal: BigDecimal)
+  val data = Seq(Data(BigDecimal.decimal(1.2f)))
+
+  "Decimal value" should "read from binary" in {
+
+    val outputFile = InMemoryOutputFile(initBufferSize = 1024)
+
+    // write
+    ParquetWriter
+      .generic(Message(Some("binary-dec"), TypedSchemaDef.decimalSchema("decimal")))
+      .writeAndClose(
+        outputFile,
+        Seq(RowParquetRecord("decimal" -> BinaryValue(Decimals.binaryFromDecimal(BigDecimal.decimal(1.2f)))))
+      )
+
+    val inputFile = outputFile.toInputFile
+
+    // read
+    Using(ParquetReader.as[Data].read(inputFile)) { readData =>
+      readData.toSeq shouldBe data
+    }
+  }
+
+  it should "read from long" in {
+
+    val outputFile = InMemoryOutputFile(initBufferSize = 1024)
+
+    // write
+    ParquetWriter
+      .generic(
+        Message(
+          Some("long-dec"),
+          SchemaDef.primitive(
+            primitiveType         = PrimitiveType.PrimitiveTypeName.INT64,
+            logicalTypeAnnotation = Some(LogicalTypeAnnotation.decimalType(4, 8))
+          )("decimal")
+        )
+      )
+      .writeAndClose(outputFile, Seq(RowParquetRecord("decimal" -> LongValue(12000L))))
+
+    val inputFile = outputFile.toInputFile
+
+    // read
+    Using(ParquetReader.as[Data].read(inputFile)) { readData =>
+      readData.toSeq shouldBe data
+    }
+  }
+
+}
