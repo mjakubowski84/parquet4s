78601d56fc4f7fcb8614aa873f49119b8df6a808
Angel Sanadinov <angel.sanadinov@lunatech.com>
2024-02-06 16:06:45 +0100

    Update fs2 partition path generation to avoid unnecessary object creation

--- fs2Benchmarks/src/main/scala/com/github/mjakubowski84/parquet4s/Fs2Benchmark.scala
+++ fs2Benchmarks/src/main/scala/com/github/mjakubowski84/parquet4s/Fs2Benchmark.scala
@@ -17,6 +17,17 @@ import scala.util.Random
 case class Embedded(fraction: Double, text: String)
 case class Record(i: Int, dict: String, embedded: Option[Embedded])
 
+case class LargeRecord(
+    i: Int,
+    dict: String,
+    embedded: Option[Embedded],
+    a: String,
+    b: String,
+    c: String,
+    d: String,
+    e: String
+)
+
 object Fs2Benchmark {
 
   private val Fractioner = 100.12
@@ -27,10 +38,11 @@ object Fs2Benchmark {
 
     // 512 * 1024
     @Param(Array("524288"))
-    var datasetSize: Int                    = _
-    var basePath: Path                      = _
-    var records: immutable.Iterable[Record] = _
-    var ioRuntime: IORuntime                = _
+    var datasetSize: Int                              = _
+    var basePath: Path                                = _
+    var records: immutable.Iterable[Record]           = _
+    var largeRecords: immutable.Iterable[LargeRecord] = _
+    var ioRuntime: IORuntime                          = _
 
     @Setup(Level.Trial)
     def setup(): Unit = {
@@ -43,6 +55,20 @@ object Fs2Benchmark {
           embedded =
             if (i % 2 == 0) Some(Embedded(1.toDouble / Fractioner, UUID.randomUUID().toString))
             else None
+        )
+      }
+      largeRecords = (1 to datasetSize).map { i =>
+        LargeRecord(
+          i    = i,
+          dict = Dict(Random.nextInt(Dict.size - 1)),
+          embedded =
+            if (i % 2 == 0) Some(Embedded(1.toDouble / Fractioner, UUID.randomUUID().toString))
+            else None,
+          a = Dict(Random.nextInt(Dict.size - 1)),
+          b = Dict(Random.nextInt(Dict.size - 1)),
+          c = Dict(Random.nextInt(Dict.size - 1)),
+          d = Dict(Random.nextInt(Dict.size - 1)),
+          e = Dict(Random.nextInt(Dict.size - 1))
         )
       }
       // using single thread in order to not measure thread syncing
@@ -145,7 +171,37 @@ object Fs2Benchmark {
     @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     def writePartitioned(): Record =
       operation.unsafeRunSync()
+  }
 
+  @State(Scope.Thread)
+  class WritePartitionedLargeRecordState extends BaseState {
+    var operation: IO[LargeRecord]      = _
+    var chunks: Seq[Chunk[LargeRecord]] = _
+
+    @Setup(Level.Trial)
+    def setup(dataset: Dataset): Unit = {
+      fetchDataset(dataset)
+      chunks = Stream.iterable(dataset.largeRecords).chunkN(16).compile.toVector
+      operation = Stream
+        .iterable(chunks)
+        .unchunks
+        .through(
+          parquet
+            .viaParquet[IO]
+            .of[LargeRecord]
+            .partitionBy(ColumnPath("a"), ColumnPath("b"), ColumnPath("c"), ColumnPath("d"), ColumnPath("e"))
+            .write(dataset.basePath)
+        )
+        .compile
+        .lastOrError
+    }
+
+    @TearDown(Level.Invocation)
+    def clearDataset(): Unit = dataset.delete()
+
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    def writePartitionedLargeRecord(): LargeRecord =
+      operation.unsafeRunSync()
   }
 
   @State(Scope.Benchmark)
@@ -203,4 +259,10 @@ class Fs2Benchmark {
   def writePartitioned(state: WritePartitionedState): Record =
     state.writePartitioned()
 
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Fork(jvmArgsAppend = Array("-Dcats.effect.stackTracingMode=disabled"))
+  def writePartitionedLargeRecord(state: WritePartitionedLargeRecordState): LargeRecord =
+    state.writePartitionedLargeRecord()
 }
