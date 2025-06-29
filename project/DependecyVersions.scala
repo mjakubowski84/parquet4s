4c3363d0e278bc2e8df013bc38508ca575b05040
Marcin Jakubowski <mjakubowski84@gmail.com>
2025-06-29 13:08:17 +0200

    Minor upgrades

--- project/DependecyVersions.scala
+++ project/DependecyVersions.scala
@@ -1,7 +1,7 @@
 object DependecyVersions {
   val parquetVersion               = "1.15.2"
   val shapelessVersion             = "2.3.13"
-  val sparkVersion                 = "3.5.5"
+  val sparkVersion                 = "3.5.6"
   val hadoopVersion                = "3.4.1"
   val slf4jVersion                 = "2.0.17"
   val logbackVersion               = "1.3.15" // stick to 1.3.x for JDK-8 compatibility
@@ -11,7 +11,7 @@ object DependecyVersions {
   val scalaCollectionCompatVersion = "2.13.0"
   val scalatestVersion             = "3.2.19"
   val mockitoVersion               = "4.11.0" // stick to 4.x for JDK-8 compatibility
-  val pekkoVersion                 = "1.1.3"
-  val jacksonVersion               = "2.19.0"
+  val pekkoVersion                 = "1.1.4"
+  val jacksonVersion               = "2.19.1"
   val testcontainersVersion        = "0.43.0"
 }
